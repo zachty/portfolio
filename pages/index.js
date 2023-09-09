8df8536407bc178a6fef2ba144fc0eefe50aebb2
Zach Byerley <zachbyerley@gmail.com>
2023-09-08 22:46:46 -0400

    made the site look a little more like its being worked on

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-// pages/index.js
+import Image from 'next/image';
+
 export default function Home({ formattedDate }) {
     return (
         <>
-            <h1>Static page</h1>
+            <h1>This site is under construction! Come back soon!</h1>
+            <Image
+                src="/construction.png"
+                alt="construction icon"
+                width={512}
+                height={512}
+            />
+            <p>
+                <a
+                    href="https://www.flaticon.com/free-icons/construction"
+                    title="construction icons"
+                >
+                    Construction icons created by Linector - Flaticon
+                </a>
+            </p>
             <p>This page is static. It was built on {formattedDate}.</p>
             <p>
-                <a href="/ssr">View a server-side rendered page.</a>
+                <a href="/ssr">View a 404 page.</a>
             </p>
         </>
     );
