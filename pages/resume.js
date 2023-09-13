a64da96cb9364a8973c618d5ab25df1dee47159e
Zach Byerley <zachbyerley@gmail.com>
2023-09-12 23:14:34 -0400

    set placeholder for resume and added less

--- pages/resume.js
+++ pages/resume.js
@@ -1,1 +1,9 @@
 //downloadable resume?
+export default function Resume() {
+    return (
+        <div>
+            <button>Download</button>
+            <p>placeholder for online resume</p>
+        </div>
+    );
+}
