0fbcf3a2d0cdf6f45b2a9eb2ff57deac8cfbf8e5
Zach Byerley <zachbyerley@gmail.com>
2023-09-13 22:43:00 -0400

    set placeholder for about me page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,2 +1,10 @@
 //page about me
 //throw in pics, hobbies, pitch, etc
+import Image from 'next/image';
+export default function Resume() {
+    return (
+        <div>
+            <p>placeholder for about me</p>
+        </div>
+    );
+}
