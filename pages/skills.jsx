9dc0829a457249e40c600b08dfa393e6e2b7ccfd
Zach Byerley <zachbyerley@gmail.com>
2023-09-15 20:57:30 -0400

    placeholder for skills page

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -1,1 +1,13 @@
 //tech stack, soft skills, hard skills
+export default function Resume() {
+    return (
+        <div>
+            <ol>
+                <lil>skill 1</lil>
+                <lil>skill 2</lil>
+                <lil>skill 3</lil>
+                <lil>skill 4</lil>
+            </ol>
+        </div>
+    );
+}
