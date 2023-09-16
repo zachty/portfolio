15ebc29d2e5cb7f641bfd8ca30ed7356cc16cacd
Zach Byerley <zachbyerley@gmail.com>
2023-09-15 20:54:17 -0400

    placeholder for projects page

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,1 +1,14 @@
 //projects i have worked on
+export default function Resume() {
+    return (
+        <div>
+            {/* TODO: make project component, class style for this lisyt */}
+            <ul>
+                <li>project1</li>
+                <li>project2</li>
+                <li>project3</li>
+                <li>project4</li>
+            </ul>
+        </div>
+    );
+}
