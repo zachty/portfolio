bf0abef27a0a6f6ae42565952affaba56e6d2884
Zach Byerley <zachbyerley@gmail.com>
2023-09-16 19:44:11 -0400

    moved skills to resume page

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -3,6 +3,13 @@ export default function Resume() {
     return (
         <div>
             <button>Download</button>
+            {/* put list of skills here */}
+            <ol>
+                <lil>skill 1</lil>
+                <lil>skill 2</lil>
+                <lil>skill 3</lil>
+                <lil>skill 4</lil>
+            </ol>
             <p>placeholder for online resume</p>
         </div>
     );
