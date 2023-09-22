74e9abca971cb414a31f6e590072e6f60dd517d6
Zach Byerley <zachbyerley@gmail.com>
2023-09-22 15:17:32 -0400

    added layout comp to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,11 @@
 import '../styles/globals.css';
-import Navbar from '../components/Navbar';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Navbar>
+        <Layout>
             <Component {...pageProps} />
-        </Navbar>
+        </Layout>
     );
 }
 
