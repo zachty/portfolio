5b335156e4116c2b348d94b612195e638adad031
Zach Byerley <zachbyerley@gmail.com>
2023-09-23 12:37:03 -0400

    placeholder for a list of projects

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,10 +1,23 @@
 //projects i have worked on
 //animate on scroll here? or maybe find some other exciting way to present the projects
-//TODO: make project component
 import Image from 'next/image';
 
+//TODO: make project component, pass each project as a prop
 //TODO: list of project objects
-const projects = [];
+const projects = [
+    {
+        title: 'Project 1',
+        description:
+            'Short description of project upon clicking image or title. Description of what I did. Bullet points from resume and what not. Any challenges and how they were overcome. Link to site/github/videos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus qui! Quibusdam minus est maiores minima excepturi tempore dolorem consectetur, quisquam, cupiditate quia accusamus, nostrum vel a velit rem! Rem.',
+        bullets: ['did this', 'did this too'],
+    },
+    {
+        title: 'Project 2',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus qui! Quibusdam minus est maiores minima excepturi tempore dolorem consectetur, quisquam, cupiditate quia accusamus, nostrum vel a velit rem! Rem.',
+        bullets: ['did this on this', 'did this as well'],
+    },
+];
 
 export default function Projects() {
     return (
