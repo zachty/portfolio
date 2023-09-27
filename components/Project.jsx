0d7746558089f86258324d5da12a8a72119cbead
Zach Byerley <zachbyerley@gmail.com>
2023-09-27 12:24:09 -0400

    created Project component

---
+++ components/Project.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Project() {
+    return (
+        <>
+            <h2>Project 1</h2>
+            <p>
+                Short description of project upon clicking image or title.
+                Description of what I did. Bullet points from resume and what
+                not. Any challenges and how they were overcome. Link to
+                site/github/videos. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Voluptate, natus qui! uibusdam minus est
+                maiores minima excepturi tempore dolorem consectetur, quisquam,
+                cupiditate quia accusamus, nostrum vel a velit rem! Rem.
+            </p>
+            <ul>
+                <li>Bullet1</li>
+                <li>Bullet2</li>
+            </ul>
+        </>
+    );
+}
