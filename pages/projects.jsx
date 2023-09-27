4518b216c2abeb8283fc68d56563e26175d8bd4d
Zach Byerley <zachbyerley@gmail.com>
2023-09-27 12:41:46 -0400

    implemented part of projects

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,6 +1,7 @@
 //projects i have worked on
 //animate on scroll here? or maybe find some other exciting way to present the projects
 import Image from 'next/image';
+import Project from '../components/Project'; //<Project proj={projects[0]}
 
 //TODO: make project component, pass each project as a prop
 //TODO: list of project objects
@@ -26,16 +27,8 @@ export default function Projects() {
             <h1>Projects</h1>
             <ol>
                 <li>
-                    <h2>Project 1</h2> Short description of project upon
-                    clicking image or title. Description of what I did. Bullet
-                    points from resume and what not. Any challenges and how they
-                    were overcome. Link to site/github/videos. Lorem ipsum dolor
-                    sit amet consectetur adipisicing elit. Voluptate, natus qui!
-                    Quibusdam minus est maiores minima excepturi tempore dolorem
-                    consectetur, quisquam, cupiditate quia accusamus, nostrum
-                    vel a velit rem! Rem.
+                    <Project proj={projects[0]} />
                 </li>
-                <li>project2</li>
                 <li>project3</li>
                 <li>project4</li>
             </ol>
